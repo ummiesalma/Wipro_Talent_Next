ae2077a1da30b0fc2a8b6c5781acf6a4691d96b0
UmmieSalma <ummiesalma457@gmail.com>
2025-07-27 16:21:26 +0530

    first commit

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic{
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("addition:"+ (a + b)+
+                           "\nsubtraction:" + (a - b) +
+                           "\nmultiplication:" + (a * b) +
+                           "\ndivision:" + (a / b) +
+                           "\nmodulus:" + (a % b));
+        
+
+    }
+
+}
+
+
